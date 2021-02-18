76a6da10c48d63dba6ef135eb4fca8746485396f
Shiv <shewashivam@gmail.com>
2021-02-18 13:24:14 -0500

    Working App

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # 10-oop
+## Employee Tracker
+## The Repository
+This repository ("`10-oop`") is where my Employee tracker app resides [Employee Tracker](https://github.com/Shiv-MS/10-oop).   The application will prompt the user for information about the team manager and then information about the team members. The user can input any number of team members, and they may be a mix of engineers and interns.
--- app.js
+++ app.js
@@ -9,27 +9,144 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const employee = [];
+const employeeID = [];
 
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+function teamBuilder() {
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+    function createBoss() {
+      console.log("Please answer the following questions to build your team!");
+      inquirer.prompt([
+        {
+          type: "input",
+          name: "bossName",
+          message: "Who's the boss?",
+        },
+        {
+          type: "input",
+          name: "bossId",
+          message: "What's the bosses id?",
+        },
+        {
+          type: "input",
+          name: "bossEmail",
+          message: "What's the bosses email?",
+        },
+        {
+          type: "input",
+          name: "bossOfficeNumber",
+          message: "What's the bosses number?",
+        }
+      ]).then(answers => {
+        const manager = new Manager(answers.bossName, answers.bossId, answers.bossEmail, answers.bossOfficeNumber);
+        employee.push(manager);
+        employeeID.push(answers.bossId);
+        theDreamTeam();
+      });
+    }
+  
+    function theDreamTeam() {
+  
+      inquirer.prompt([
+        {
+          type: "list",
+          name: "teammateType",
+          message: "Which type of team member would you like to add?",
+          choices: [
+            "Engineer",
+            "Intern",
+            "No more team members"
+          ]
+        }
+      ]).then(userChoice => {
+        switch(userChoice.teammateType) {
+        case "Engineer":
+          addEngineer();
+          break;
+        case "Intern":
+          addIntern();
+          break;
+        default:
+          buildTeam();
+        }
+      });
+    }
+  
+    function addEngineer() {
+      inquirer.prompt([
+        {
+          type: "input",
+          name: "engineerName",
+          message: "What is the engineer's name?",
+        },
+        {
+          type: "input",
+          name: "engineerId",
+          message: "What is the engineer's id?",
+        },
+        {
+          type: "input",
+          name: "engineerEmail",
+          message: "What is the engineer's email?",
+        },
+        {
+          type: "input",
+          name: "engineerGithub",
+          message: "What is the engineer's GitHub username?",
+        }
+      ]).then(answers => {
+        const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub);
+        employee.push(engineer);
+        employeeID.push(answers.engineerId);
+        theDreamTeam();
+      });
+    }
+  
+    function addIntern() {
+      inquirer.prompt([
+        {
+          type: "input",
+          name: "internName",
+          message: "What is your intern's name?",
+        },
+        {
+          type: "input",
+          name: "internId",
+          message: "What is your intern's id?",
+        },
+        {
+          type: "input",
+          name: "internEmail",
+          message: "What is your intern's email?",
+        },
+        {
+          type: "input",
+          name: "internSchool",
+          message: "What is your intern's school?",
+        }
+      ]).then(answers => {
+        const intern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.internSchool);
+        employee.push(intern);
+        employeeID.push(answers.internId);
+        theDreamTeam();
+      });
+    }
+  
+    function buildTeam() {
+      // Create the output directory if the output path doesn't exist
+      if (!fs.existsSync(OUTPUT_DIR)) {
+        fs.mkdirSync(OUTPUT_DIR)
+      }
+      fs.writeFileSync(outputPath, render(employee), "utf-8");
+    }
+  
+    createBoss();
+  
+  }
+  
+  
+  teamBuilder();
